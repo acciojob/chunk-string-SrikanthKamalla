9fc8997a243f1c755979780ee9808137dd5fd5b5
Srikanth Kamalla <113651024+SrikanthKamalla@users.noreply.github.com>
2025-01-12 13:38:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function stringChop(str, size) {
   // your code here
+	let n=str.length;
+	let size=5;
+	let ans=[];
+	let i = 0;
+	while ( i < n) {
+	    let k='';
+	    let j = 0;
+	    while ( i < n && j < size) {
+	        
+	        k+=str[i];
+	        i++;
+	        j++
+	        
+	    }
+	    ans.push(k);
+	}
+	return ans;
 }
 
 // Do not change the code below
